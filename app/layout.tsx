563088a399434a1eb4d8cc5128665a87f3777d1e
ali-mango <julesalysa@gmail.com>
2026-04-07 13:48:48 +0800

    updated font, used plus jakarta sans

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "UnliTravel Loan Services",
@@ -13,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={plusJakartaSans.className}>{children}</body>
     </html>
   );
 }
